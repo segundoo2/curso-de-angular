4aee6931e43bd7a8ee78eb2af23dea54834faf52
Edilson Segundo <edilsonsilvaa13@gmail.com>
2024-12-21 19:53:05 -0300

    Remove duplicação de código do botão adquirir e refatora código scss.

--- Projeto-Components-Seção-3/src/app/cards/cards.module.ts
+++ Projeto-Components-Seção-3/src/app/cards/cards.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 import { CardButtonComponent } from './card-button/card-button.component';
-import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 import { CardCancelButtomComponent } from './card-cancel-buttom/card-cancel-buttom.component';
 
 
@@ -14,7 +13,6 @@ import { CardCancelButtomComponent } from './card-cancel-buttom/card-cancel-butt
     CardComponent,
     CardButtonComponent,
     CardRoxoComponent,
-    CardRoxoButtonComponent,
     CardCancelButtomComponent
   ],
   imports: [
@@ -23,7 +21,6 @@ import { CardCancelButtomComponent } from './card-cancel-buttom/card-cancel-butt
   exports: [
     CardComponent,
     CardRoxoComponent,
-    CardCancelButtomComponent
   ]
 })
 export class CardsModule { }
