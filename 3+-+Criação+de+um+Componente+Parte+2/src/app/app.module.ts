c528fe2c995d41909ad57d8a4c1a811a70c24f6a
Edilson Segundo <edilsonsilvaa13@gmail.com>
2024-12-23 12:19:14 -0300

    não sei

---
+++ 3+-+Criação+de+um+Componente+Parte+2/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
